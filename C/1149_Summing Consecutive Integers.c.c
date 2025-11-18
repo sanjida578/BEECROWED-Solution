70def215a88feb6e99698406f1726947242949cf
Sanjida Aktar <mst.sanjida.aktar.cnj.bd@gmail.com>
2025-11-18 11:27:35 +0700

    Add files via upload

---
+++ C/1149_Summing Consecutive Integers.c.c
@@ -0,0 +1,18 @@
+// Summing consecutive integers
+//
+#include<stdio.h>
+int main()
+{
+    int a,n,sum=0;
+    scanf("%d%d",&a,&n);
+    while(n<=0)
+    {
+        scanf("%d",&n);
+    }
+    for(int i=0;i<n;i++)
+    {
+        sum+=a+i;
+    }
+    printf("%d\n",sum);
+    return 0;
+}
