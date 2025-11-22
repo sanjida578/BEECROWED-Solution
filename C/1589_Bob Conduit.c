ecd6c339105f05a1a5976e60c8548c602a0122bc
Sanjida Aktar <mst.sanjida.aktar.cnj.bd@gmail.com>
2025-11-22 23:05:26 +0700

    Add files via upload

---
+++ C/1589_Bob Conduit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t,r1,r2;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++)
+    {
+        scanf("%d%d",&r1,&r2);
+        int sum=r1+r2;
+        printf("%d\n",sum);
+    }
+    return 0;
+}
